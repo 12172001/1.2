e7a2be93d4e6d08ee0b29c6ddd955cc3f36b18f2
Khanisa <62868383+12172001@users.noreply.github.com>
2020-04-03 20:34:12 +0300

    Add files via upload

---
+++ 3.5.py
@@ -0,0 +1,16 @@
+x = float(input())
+if x - int(x) == 0.5:
+    x += 0.1
+print(round(x))
+
+
+x = float(input())
+if x - int(x) == 0.5:
+    x += 0.2
+print(round(x))
+
+
+x = float(input())
+if x - int(x) == 0.5:
+    x += 0.3
+print(round(x))
